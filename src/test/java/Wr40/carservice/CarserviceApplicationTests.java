d49fa53b7e64e1e3d06932e898c9c3947f62efb3
Tomasz Borowski <bortom112233@gmail.com>
2022-11-21 21:11:23 +0100

    spring init

---
+++ src/test/java/Wr40/carservice/CarserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package Wr40.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
