bf6c65e97b31eb75a76d560b80d1e2b448a4a5fc
Tomasz Borowski <bortom112233@gmail.com>
2022-11-25 11:01:24 +0100

    replace init db connection to container (Liquibase migration)

--- src/test/java/Wr40/carservice/CarserviceApplicationTests.java
+++ src/test/java/Wr40/carservice/CarserviceApplicationTests.java
@@ -3,7 +3,7 @@ package Wr40.carservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DatabaseConfig.class)
 class CarserviceApplicationTests {
 
 	@Test
