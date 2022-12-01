dc0866ea388f628dd409550848622efe4a9cc72a
AlexanderPanchyshyn <alexandr.panchyshyn34@gmail.com>
2022-12-01 16:32:34 +0100

    deleting NoArgsConstructor
    with this annotation modelmapper isn`t working

--- src/main/java/Wr40/cardiary/controller/UserController.java
+++ src/main/java/Wr40/cardiary/controller/UserController.java
@@ -5,14 +5,12 @@ import Wr40.cardiary.model.entity.User;
 import Wr40.cardiary.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@NoArgsConstructor
 @RequestMapping("/api/cardiary/user")
 public class UserController {
 
