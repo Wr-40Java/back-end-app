600aa86f9ab897bdfd7eee68d39d3d09039dd638
AlexanderPanchyshyn <alexandr.panchyshyn34@gmail.com>
2022-12-01 12:47:39 +0100

    adding email validation to save user

--- src/main/java/Wr40/cardiary/service/UserService.java
+++ src/main/java/Wr40/cardiary/service/UserService.java
@@ -1,10 +1,14 @@
 package Wr40.cardiary.service;
 
+import Wr40.cardiary.exception.UserAlreadyExistedException;
+import Wr40.cardiary.exception.WrongEmailAddressException;
 import Wr40.cardiary.model.entity.User;
 import Wr40.cardiary.repo.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -13,7 +17,16 @@ public class UserService {
 
     UserRepository userRepository;
 
+    String emailValidationRegexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
     public User saveUser(User user) {
+        if (userRepository.findByUserName(user.getUsername()).isPresent()) {
+            throw new UserAlreadyExistedException();
+        }
+        if (!Objects.equals(user.getEmail(), emailValidationRegexPattern)) {
+            throw new WrongEmailAddressException();
+        }
         return userRepository.save(user);
     }
 }
