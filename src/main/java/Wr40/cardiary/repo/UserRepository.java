ec4319b1e93e02b2750981ac47938508fac43a66
AlexanderPanchyshyn <alexandr.panchyshyn34@gmail.com>
2022-12-01 13:14:33 +0100

    fixing repo method

--- src/main/java/Wr40/cardiary/repo/UserRepository.java
+++ src/main/java/Wr40/cardiary/repo/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findUserByUserName(String username);
+    Optional<User> findUserByUsername(String username);
 }
