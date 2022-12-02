e755bdab21249deca4b41ca0a95b5c270ae8424a
Maciej Matusz <maciej.matusz.szczecin@gmail.com>
2022-12-02 14:34:21 +0100

    add GetAllUsers

--- src/main/java/Wr40/cardiary/controller/UserController.java
+++ src/main/java/Wr40/cardiary/controller/UserController.java
@@ -1,10 +1,16 @@
 package Wr40.cardiary.controller;
 
+import Wr40.cardiary.model.entity.User;
 import Wr40.cardiary.service.UserService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -14,4 +20,10 @@ public class UserController {
     UserService userService;
 
     ModelMapper modelMapper;
+
+    @GetMapping("/get")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
 }
--- src/main/java/Wr40/cardiary/service/UserService.java
+++ src/main/java/Wr40/cardiary/service/UserService.java
@@ -1,9 +1,13 @@
 package Wr40.cardiary.service;
 
+import Wr40.cardiary.exception.UserNotFoundException;
+import Wr40.cardiary.model.entity.User;
 import Wr40.cardiary.repo.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -11,4 +15,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String username){
+        return userRepository.findUserByUsername(username).orElseThrow(UserNotFoundException::new);
+    }
 }
