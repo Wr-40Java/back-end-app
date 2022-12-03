f912a13b3dcd37b34630750e3860bbb834d05386
Maciej Mackowiak <mmackowiak08@gmail.com>
2022-12-03 19:42:26 +0100

    Fix to pass build_check

--- src/test/java/Wr40/cardiary/controller/CarControllerTest.java
+++ src/test/java/Wr40/cardiary/controller/CarControllerTest.java
@@ -1,9 +1,12 @@
 package Wr40.cardiary.controller;
 
+import Wr40.cardiary.DatabaseConfig;
 import org.junit.Before;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
