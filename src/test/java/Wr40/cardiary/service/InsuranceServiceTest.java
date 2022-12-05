969d0ed6b73919f3c1f1bb7078800c552001021a
Tomasz Borowski <bortom112233@gmail.com>
2022-12-05 15:08:34 +0100

    delete InsuranceType - fix test

--- src/test/java/Wr40/cardiary/service/InsuranceServiceTest.java
+++ src/test/java/Wr40/cardiary/service/InsuranceServiceTest.java
@@ -276,6 +276,7 @@ public class InsuranceServiceTest {
 
         InsuranceCompany insuranceCompany = new InsuranceCompany();
         insuranceCompany.setName("NewSafeLvl").setDescription("For me the best so far").setPhoneNumber(19027883L).setInsuranceType(insuranceType);
+        car.addInsuranceCompany(insuranceCompany);
 
         Mockito.when(carRepository.findByVINnumber(VINNumber)).thenReturn(Optional.of(car));
         Mockito.when(insuranceRepository.findById(InsCompId)).thenReturn((Optional.of(insuranceCompany)));
@@ -284,7 +285,6 @@ public class InsuranceServiceTest {
 //        HashSet<InsuranceCompany> objects = new HashSet<>();
 //        insuranceCompany.setInsuranceType(insuranceType);
 //        objects.add(insuranceCompany);
-        car.addInsuranceCompany(insuranceCompany);
         Mockito.when(carRepository.save(car)).thenReturn(car);
 
         Mockito.when(modelMapper.map(insuranceCompany, InsuranceCompanyWithTypeDTO.class)).thenReturn(insuranceCompanyWithTypeDTO);
