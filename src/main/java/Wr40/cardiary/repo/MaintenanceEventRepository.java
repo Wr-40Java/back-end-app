46ec5a51c3e444f418b17031b1f670314d6bcea2
tamtejszystan <mtustanowski@gmail.com>
2022-12-07 12:37:52 +0100

    statistics - maintenance repos

---
+++ src/main/java/Wr40/cardiary/repo/MaintenanceEventRepository.java
@@ -0,0 +1,25 @@
+package Wr40.cardiary.repo;
+
+import Wr40.cardiary.model.entity.MaintenanaceEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MaintenanceEventRepository extends JpaRepository<MaintenanaceEvent, Long> {
+    @Query(value = "select CONCAT (round(SUM(maintenance_event.cost),2), ';', maintenance_event.company_responsible_for_name, ';', maintenance_event.description, ';', maintenance_event_timestamp) AS \"info\" from maintenance_history left join maintenance_event on maintenance_history.maintenance_event_id = maintenance_event.id\n" +
+            "group by maintenance_event.description, maintenance_event.company_responsible_for_name\n" +
+            "order by round(SUM(maintenance_event.cost),2)", nativeQuery = true)
+
+    List<String> getMaintenanceStatsWithSumOfExpenses();
+
+    @Query(value = "select CONCAT (round(AVG(maintenance_event.cost),2), ';' maintenance_event.description, ';', maintenance_event.company_responsible_for_name) AS \"info\" from maintenance_history left join maintenance_event on maintenance_history.maintenance_event_id = maintenance_event.id\n" +
+            "group by maintenance_event.description, maintenance_event.company_responsible_for_name\n" +
+            "order by round(AVG(maintenance_event.cost),2)", nativeQuery = true)
+
+    List<String> getMaintenanceStatsWithAvgOfExpenses();
+
+
+}
